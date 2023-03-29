ff00839d5b93bc184650ca8f54ed55d8c6e20e55
westhide <mr.westhide@gmail.com>
2023-03-29 10:14:32 +0800

    fix(pai-shared): Error

--- crates/shared/src/error/mod.rs
+++ crates/shared/src/error/mod.rs
@@ -4,9 +4,11 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error(transparent)]
     IO(#[from] io::Error),
+
     #[error("Lexer: {0}")]
-    Lexer(),
+    Lexer(String),
 }
 
 pub type Result<T> = result::Result<T, Error>;
